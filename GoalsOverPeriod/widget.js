a55cde7312dcefa9d3b565245f57655e40ea407d
Lx <lukasz@klis.org.pl>
2018-11-23 20:38:30 +0100

    Goals over periods - session, weekly, monthly, all-time

---
+++ GoalsOverPeriod/widget.js
@@ -0,0 +1,30 @@
+let index, goal, fieldData;
+
+window.addEventListener('onWidgetLoad', function (obj) {
+    fieldData = obj.detail.fieldData;
+    goal = fieldData["goal"];
+    $("#goal").html(goal);
+    index = fieldData['eventType'] + "-" + fieldData['eventPeriod'];
+    if (fieldData['eventType'] === 'cheer' || fieldData['eventType'] === 'tip') {
+        count = obj["detail"]["session"]["data"][index]['amount'];
+    } else {
+        count = obj["detail"]["session"]["data"][index]['count'];
+    }
+    updateBar(count);
+
+});
+
+window.addEventListener('onSessionUpdate', function (obj) {
+    if (fieldData['eventType'] === 'cheer' || fieldData['eventType'] === 'tip') {
+        count = obj["detail"]["session"]["data"][index]['amount'];
+    } else {
+        count = obj["detail"]["session"]["data"][index]['count'];
+    }
+    updateBar(count);
+});
+
+function updateBar(count) {
+    let percentage = Math.min(100, (count / goal * 100).toPrecision(3));
+    $("#bar").css('width', percentage + "%");
+    $("#count").html(count);
+}
