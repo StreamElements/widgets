a703839f03b5c6d118206e17a00894b4229f6eb2
Lx <lukasz@klis.org.pl>
2018-12-04 09:23:54 +0100

    Fixed problem when widget didn't play with real events and played with replay and test events Added comments about redeemable items (due to complexity it is not in fields but in JS code)

--- NanoleafLightingOnEvents/widget.js
+++ NanoleafLightingOnEvents/widget.js
@@ -1,4 +1,11 @@
 let fieldData, url;
+
+//List of redeemable items names and effects for them - th
+// For example, if your item is named "Disco Lights", add "DiscoLights":
+// "DiscoLights": {
+//      "effect": "Nemo",
+//      "time": 2,
+// },
 let items = {
     "SETshirt": {
         "effect": "Nemo",
@@ -12,24 +19,27 @@ let items = {
 
 window.addEventListener('onEventReceived', function (obj) {
 
-    if (typeof obj.detail.event.listener === "undefined") return;
-    const listener = obj.detail.event.listener.split("-");
+    if (typeof obj.detail.event.itemId !== "undefined") {
+        obj.detail.listener = "redemption-latest"
+    }
+    if (typeof obj.detail.listener === "undefined") return;
+    const listener = obj.detail.listener.split("-");
     if (typeof listener === "undefined") return;
     if (listener[1] !== "latest") return;
     let item;
     if (listener[0] === "redemption") {
-        item = obj.detail.event.event.item;
+        item = obj.detail.event.item;
         item = item.replace(/\W/g, '');
     }
     light(listener[0], item);
 });
 window.addEventListener('onWidgetLoad', function (obj) {
     fieldData = obj.detail.fieldData;
-    url = 'http://' + fieldData['ip'] + ':16021/api/v1/' + fieldData['token'] + '/effects'; // your api url
+    url = 'http://' + fieldData['ip'] + '/api/v1/' + fieldData['token'] + '/effects'; // your api url
 });
 
 function light(event, name = "") {
-    let effect = "", time = 0;
+    let effect = "", time;
     if (event === "redemption") {
         if (typeof items[name] === "undefined") return;
         effect = items[name]["effect"];
@@ -38,7 +48,9 @@ function light(event, name = "") {
         effect = fieldData[event + "Effect"];
         time = fieldData[event + "Time"];
     }
-    if (effect === "" || time === 0) return;
+
+    if (effect === "") return;
+
     $.ajax({
         url: url,
         method: 'PUT',
