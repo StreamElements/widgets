6037c93aab4a53117c46120db175672aa7190aea
Lx <lukasz@klis.org.pl>
2018-12-16 10:09:40 +0100

    Added subscriber goal Added localization for currency display

--- GoalsOverPeriod/widget.js
+++ GoalsOverPeriod/widget.js
@@ -1,20 +1,24 @@
-let index, goal, fieldData, currency;
+let index, goal, fieldData, currency, userLocale;
 
 window.addEventListener('onWidgetLoad', function (obj) {
     fieldData = obj.detail.fieldData;
     goal = fieldData["goal"];
+    userLocale = fieldData["userLocale"];
     currency = obj["detail"]["currency"]["code"];
     index = fieldData['eventType'] + "-" + fieldData['eventPeriod'];
+    if (fieldData['eventType'] === "subscriber-points") {
+        index = fieldData['eventType'];
+    }
     count = 0;
     if (typeof obj["detail"]["session"]["data"][index] !== 'undefined') {
-        if (fieldData['eventType'] === 'cheer' || fieldData['eventType'] === 'tip') {
+        if (fieldData['eventType'] === 'cheer' || fieldData['eventType'] === 'tip' || fieldData['eventType'] === 'subscriber-points') {
             count = obj["detail"]["session"]["data"][index]['amount'];
         } else {
             count = obj["detail"]["session"]["data"][index]['count'];
         }
     }
     if (fieldData['eventType'] === 'tip') {
-        $("#goal").html(goal.toLocaleString(undefined, {style: 'currency', currency: currency}));
+        $("#goal").html(goal.toLocaleString(userLocale, {style: 'currency', currency: currency}));
     } else {
         $("#goal").html(goal);
     }
@@ -23,7 +27,7 @@ window.addEventListener('onWidgetLoad', function (obj) {
 
 window.addEventListener('onSessionUpdate', function (obj) {
     if (typeof obj["detail"]["session"][index] !== 'undefined') {
-        if (fieldData['eventType'] === 'cheer' || fieldData['eventType'] === 'tip') {
+        if (fieldData['eventType'] === 'cheer' || fieldData['eventType'] === 'tip' || fieldData['eventType'] === 'subscriber-points') {
             count = obj["detail"]["session"][index]['amount'];
         } else {
             count = obj["detail"]["session"][index]['count'];
@@ -36,7 +40,7 @@ function updateBar(count) {
     let percentage = Math.min(100, (count / goal * 100).toPrecision(3));
     $("#bar").css('width', percentage + "%");
     if (fieldData['eventType'] === 'tip') {
-        count = count.toLocaleString(undefined, {style: 'currency', currency: currency})
+        count = count.toLocaleString(userLocale, {style: 'currency', currency: currency})
     }
     $("#count").html(count);
 
