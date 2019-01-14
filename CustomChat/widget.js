76efdf5f0dee66b35c3d984896ba294c6b610f63
lx <lukasz@klis.org.pl>
2019-01-14 20:38:12 +0100

    Custom chat initial

---
+++ CustomChat/widget.js
@@ -0,0 +1,99 @@
+let totalMessages = 0, messagesLimit = 0, nickColor = "user";
+let animationIn = 'bounceIn';
+let animationOut = 'bounceOut';
+let hideAfter = 60;
+window.addEventListener('onEventReceived', function (obj) {
+    console.log(obj.detail);
+    if (obj.detail.listener !== "message") return;
+    let data = obj.detail.event.data;
+    let message = attachEmotes(data);
+    let badges = "", badge;
+    for (var i = 0; i < data.badges.length; i++) {
+        badge = data.badges[i];
+        badges += `<img alt="" src="${badge.url}" class="badge"> `;
+    }
+    let username = data.displayName;
+    if (nickColor === "user") {
+        const color = data.displayColor;
+        username = `<span style="color:${color}">${username}</span>`;
+    }
+    addEvent(username, badges, message, data.isAction);
+});
+
+window.addEventListener('onWidgetLoad', function (obj) {
+    const fieldData = obj.detail.fieldData;
+    animationIn = fieldData.animationIn;
+    animationOut = fieldData.animationOut;
+    hideAfter = fieldData.hideAfter;
+    messagesLimit = fieldData.messagesLimit;
+    nickColor = fieldData.nickColor;
+});
+
+function attachEmotes(message) {
+    let text = message.text;
+    let data = message.emotes;
+    return text
+        .replace(
+            /([^\s]*)/gi,
+            function (m, key) {
+                var result = data.filter(emote => {
+                    return emote.name === key
+                });
+                if (typeof result[0] !== "undefined") {
+                    let url = result[0]['urls'][4];
+                    return `<img alt="" src="${url}" class="emote"/>`;
+                } else return key;
+
+            }
+        );
+}
+
+function addEvent(username, badges, message, isAction) {
+    totalMessages += 1;
+    let actionClass = "";
+    if (isAction) {
+        actionClass = "action";
+    }
+    const element = $.parseHTML(`
+    <div class="message-row {animationIn} animated" id="msg-${totalMessages}">
+        <div class="user-box ${actionClass}">${badges}${username}</div>
+        <div class="user-message ${actionClass}">${message}</div>
+    </div>`);
+    if (hideAfter !== 999) {
+        $(element).appendTo('.main-container').delay(hideAfter * 1000).queue(function () {
+            $(this).removeClass(animationIn).addClass(animationOut).delay(500).queue(function () {
+                $(this).remove()
+            }).dequeue();
+        });
+    } else {
+        $(element).appendTo('.main-container');
+    }
+
+    if (totalMessages > messagesLimit) {
+        removeRow(totalMessages - messagesLimit);
+    }
+}
+
+function removeRow(id) {
+    if (!$(`#msg-${id}`).length) {
+        return;
+    }
+    if (animationOut !== "none" || !$(`#msg-${id}`).hasClass(animationOut)) {
+        if (hideAfter !== 999) {
+            $(`#msg-${id}`).dequeue();
+        } else {
+            $(`#msg-${id}`).addClass(animationOut).delay(500).queue(function () {
+                $(this).remove().dequeue()
+            });
+
+        }
+        return;
+    }
+
+    $(`#msg-${id}`).animate({
+        height: 0,
+        opacity: 0
+    }, 'slow', function () {
+        $(`#msg-${id}`).remove();
+    });
+}
