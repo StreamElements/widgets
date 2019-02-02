2564715a9712c981d4a3e23124f6260fc0eeaaf4
lx <lukasz@klis.org.pl>
2019-02-02 20:19:56 +0100

    Boxes are created from JS array Define text appearing in widget in easier way

--- TopEventsRotator/widget.js
+++ TopEventsRotator/widget.js
@@ -1,3 +1,29 @@
+let elements = [
+        {
+            type: "tip-session-top-donator",
+            icon: "fa-money-bill",
+            message: "Top Tipper: {name} - ${amount}",
+            emptyMessage: "No tips yet",
+            factor: "amount", //if unset, 0 or empty string - display emptyMessage
+        },
+        {
+            type: "cheer-session-top-donator",
+            icon: "fa-gem",
+            message: "Top cheerer: {name} - {amount} Bits",
+            emptyMessage: "No cheers yet",
+            factor: "amount",
+        },
+        {
+            type: "subscriber-latest",
+            icon: "fa-people",
+            message: "Most recent sub: {name} (X{amount})",
+            emptyMessage: "No subs yet",
+            factor: "name",
+        },
+    ]
+;
+
+
 let userCurrency;
 let userLocale = "en-US";
 let animationIn = 'bounceIn';
@@ -11,11 +37,6 @@ let timeOut = 500;
 let delay = 5000;
 let timeBetween = 500;
 let slideTime = timeIn + timeDisplay + timeOut + timeBetween;
-$(document).ready(function () {
-    box = $(".mySlides");
-    amount = box.length;
-    showSlide(next);
-});
 
 
 function showSlide(i) {
@@ -42,11 +63,26 @@ function showSlide(i) {
 
 }
 
+function parseData(data) {
+    elements.forEach(function (element) {
+        let text = "";
+        if (typeof data[element.type][element.factor] === "undefined" || data[element.type][element.factor] === 0 || data[element.type][element.factor] === "") {
+            text = element.emptyMessage;
+        } else {
+            text = element.message.replace(
+                /{(\w*)}/g,
+                function (m, key) {
+                    return data[element.type].hasOwnProperty(key) ? data[element.type][key] : "";
+                }
+            );
+        }
+        $("#" + element.type).html(text);
+    });
+}
+
 window.addEventListener('onSessionUpdate', e => {
     const data = e.detail.session;
-    $("#top-donator").text(data["tip-session-top-donator"]["name"] + " - " + data["tip-session-top-donator"]["amount"]);
-    $("#top-cheerer").text(data["cheer-session-top-donator"]["name"] + " - " + data["cheer-session-top-donator"]["amount"]);
-    $("#recent-donator").val(data["tip-latest"]["name"] + ": " + data["tip-latest"]["amount"]);
+    parseData(data);
 });
 
 window.addEventListener('onWidgetLoad', function (obj) {
@@ -62,8 +98,25 @@ window.addEventListener('onWidgetLoad', function (obj) {
     userCurrency = obj.detail.currency;
 
     slideTime = timeIn + timeDisplay + timeOut;
-    $("#recent-donator").text(data["tip-latest"]["name"] + " - " + data["tip-latest"]["amount"]);
-    $("#top-donator").text(data["tip-session-top-donator"]["name"] + " - " + data["tip-session-top-donator"]["amount"]);
-    $("#top-cheerer").text(data["cheer-session-top-donator"]["name"] + " - " + data["cheer-session-top-donator"]["amount"]);
+    let duplicateCheck = [];
+    elements.forEach(function (element) {
+        if (duplicateCheck.indexOf(element.type) === -1) {
+            $(".container").append(`
+    <div class="mySlides">
+        <div class="icon">
+            <span class="fa ${element.icon}"> </span>
+        </div>
+        <div class="username">
+           <span id="${element.type}"></span>
+        </div>
+    </div>`);
+            duplicateCheck.push(element.type);
+        }
 
+    });
+    box = $(".mySlides");
+    parseData(data);
+
+    amount = box.length;
+    showSlide(next);
 });
