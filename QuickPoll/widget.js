d6206477f0b99c0ea5910bc955fb842a8f8aacf1
lx <lukasz@klis.org.pl>
2019-03-21 15:35:57 +0100

    Possibility to display goals from goals tab fix

---
+++ QuickPoll/widget.js
@@ -0,0 +1,95 @@
+let userOptions = {};
+let users = [];
+let votes = {a: 0, b: 0};
+let isActive = false;
+let time = 0;
+let interval;
+window.addEventListener('onEventReceived', function (obj) {
+        if (obj.detail.listener !== 'message') return;
+
+        let data = obj.detail.event.data;
+        let message = data['text'];
+
+        let user = data['nick'];
+        let userstate = {
+            'mod': parseInt(data.tags.mod),
+            'sub': parseInt(data.tags.subscriber),
+            'vip': (data.tags.badges.indexOf("vip") !== -1),
+            'badges': {
+                'broadcaster': (user === userOptions['channelName']),
+            }
+
+        };
+        if (((userstate.mod && userOptions['managePermissions'] === 'mods') || userstate.badges.broadcaster) && message.startsWith(userOptions['startCommand'])) {
+            if (message.startsWith(userOptions['startCommand']) && !isActive) {
+
+                users = [];
+                votes = {a: 0, b: 0};
+                isActive = true;
+                $("#optiona").css("height", '50%');
+                $("#optionb").css("height", '50%');
+                $(".main-container").fadeIn();
+                time = userOptions['pollTime'];
+                interval = setInterval(countDown, 1000);
+                return;
+            }
+
+        }
+
+        if (!isActive) return;
+        if (userOptions['onlyUniqueUsers'] === "yes" && users.indexOf(user) !== -1) return false;
+        if (userOptions['participants'] === "subs" && !userstate.sub) return false;
+        if (message.indexOf(userOptions['optionA']) > -1) {
+
+            vote('a', user);
+        } else if (message.indexOf(userOptions['optionB']) > -1) {
+
+            vote('b', user);
+        }
+
+    }
+);
+
+function vote(option, username) {
+    users.push(username);
+    votes[option]++;
+    let sizea = Math.round(votes['a'] / (votes['a'] + votes['b']) * 100);
+    let sizeb = Math.round(votes['b'] / (votes['a'] + votes['b']) * 100);
+    $("#optiona").css("height", `${sizea}%`);
+    $("#optionb").css("height", `${sizeb}%`);
+}
+
+function countDown() {
+    time--;
+    $("#timer").html(time);
+    if (time < 1) {
+        clearInterval(interval);
+        announceResults();
+    }
+}
+
+function announceResults() {
+    let message = userOptions['chatDecided'] + " ";
+    if (votes['a'] === votes['b']) {
+        message = userOptions['chatDidntDecide'];
+    } else if (votes['a'] > votes['b']) {
+        message += userOptions['optionA'];
+    } else {
+        message += userOptions['optionB'];
+    }
+    let myAudio = new Audio('http://api.streamelements.com/kappa/v2/speech?voice=' + userOptions['voice'] + '&text=' + encodeURI(message));
+    myAudio.addEventListener('ended', function () {
+
+        $(".main-container").fadeOut();
+        isActive = false;
+    }, false);
+    myAudio.volume = parseInt(userOptions['volume']) / 100;
+    myAudio.play();
+
+}
+
+window.addEventListener('onWidgetLoad', function (obj) {
+    userOptions = obj['detail']['fieldData'];
+    userOptions['channelName'] = obj['detail']['channel']['username'];
+});
+
