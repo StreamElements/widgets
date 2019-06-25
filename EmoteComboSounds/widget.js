45eeef966b872f47e646c389f16f6b37844a0e54
lx <lukasz@klis.org.pl>
2019-06-25 09:18:49 +0200

    Fixing issues with video files

--- EmoteComboSounds/widget.js
+++ EmoteComboSounds/widget.js
@@ -1,14 +1,15 @@
 // URLs in userConfig are not real, please replace them with your URLs for sounds/images
-// Sound files are mandatory, image files are optional
+// Sound files are mandatory (but can be muted), image/video files are optional
+
 let globalCooldown = 60; //seconds
 let userConfig = [
     {
         emote: "OMEGALUL",
         amount: 3,
-        soundFile: "https://cdn.streamelements.com/uploads/OMEGALUL.mp3",
+        soundFile: "https://cdn.streamelements.com/uploads/df450eef-0cf3-4b05-b8c7-bea98a1cad07.ogg",
         imageFile: "",
-        videoFile: "https://cdn.streamelements.com/uploads/OMEGALUL_ANIMATED.webm",
-        volume: 0,
+        videoFile: "",
+        volume: 60,
         timeout: 10, //seconds for triggering combo (amount occurrences within timeout seconds)
         cooldown: 240, //seconds
         caseSensitive: true,
@@ -35,7 +36,6 @@ window.addEventListener('onEventReceived', function (obj) {
     let message = data["text"];
     let words = message.split(" ");
     let results = words.filter(value => -1 !== emoticons.indexOf(value.toLowerCase()));
-    console.log(results);
     results = Array.from(new Set(results)); //getting unique words
     for (let i in results) {
         index = userConfig.findIndex(x => x.emote.toLowerCase() === results[i].toLowerCase());
@@ -57,31 +57,38 @@ function checkPlay(index) {
         if (userConfig[index]['timer'] === 0) {
             userConfig[index]['timer'] = userConfig[index]['timeout'];
         }
-        if (cooldown>0) return;
+        if (cooldown > 0) return;
         if (userConfig[index]['counter'] >= userConfig[index]['amount']) {
             userConfig[index]['cooldownEnd'] = (Date.now() / 1000) + sound.cooldown;
             let tmpaudio = new Audio(sound.soundFile);
             tmpaudio.onloadeddata = function () {
-                console.log(`adding ${index} to queue after ${tmpaudio.duration}`);
-
+                //console.log(`adding ${index} to queue after ${tmpaudio.duration}`);
                 queue
                     .queue(function () {
                         let audio = new Audio(sound.soundFile);
                         audio.volume = sound.volume * .01;
                         audio.play();
+                        let duration = tmpaudio.duration;
                         if (sound.videoFile.length > 10) {
-                            $("#video").attr('src', + sound.videoFile);
-                            setTimeout(function () {
-                                $("#video").attr('src', '');
-                            }, tmpaudio.duration * 1000);
+
+                            let video = document.getElementById("videoHolder");
+                            video.src = sound.videoFile;
+                            video.onloadedmetadata = function () {
+                                duration = Math.max(tmpaudio.duration, video.duration);
+                                video.play();
+                                setTimeout(function () {
+                                    video.pause();
+                                    video.src = null;
+                                }, duration * 1000);
+                            }
                         }
                         if (sound.imageFile.length > 10) {
                             $("#image").css('background-image', 'url(' + sound.imageFile + ')');
                             setTimeout(function () {
                                 $("#image").css('background-image', '');
-                            }, tmpaudio.duration * 1000);
+                            }, duration * 1000);
                         }
-                        $(this).delay(tmpaudio.duration * 1000);
+                        $(this).delay(duration * 1000);
                         $(this).dequeue();
                     });
 
