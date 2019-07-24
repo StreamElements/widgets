425e92c92895558b5da2e75286ed1f84521a3f5e
lx <lukasz@klis.org.pl>
2019-07-24 19:00:31 +0200

    Dynamic font size for Custom Labels

--- CustomLabels/widget.js
+++ CustomLabels/widget.js
@@ -1,38 +1,57 @@
-let userCurrency,userLocale,eventType;
+let userCurrency, userLocale, eventType;
+let height;
+let resize = () => {
+    console.log();
+    if ("{{dynamicFontSize}}" === "dynamic") {
+        setTimeout(() => {
+            fitty('.main-container', {
+                minSize: 12,
+                maxSize: height
+            });
+        }, 5);
+    }
+};
 
 window.addEventListener('onSessionUpdate', function (obj) {
     const data = obj.detail.session;
     if (!data[eventType]["name"].length) return;
-    if (eventType.indexOf("tip")!=-1){
-        $(".main-container").html(data[eventType]["name"]+" "+data[eventType]["amount"].toLocaleString(userLocale,{style: 'currency',currency:userCurrency}));
-    }else if (eventType.indexOf("sub")!=-1 || eventType.indexOf("cheer")!=-1){
-        $(".main-container").html(data[eventType]["name"]+" X"+data[eventType]["amount"]);
-    }else if (eventType.indexOf("raid")!=-1 || eventType.indexOf("host")!=-1){
-        $(".main-container").html(data[eventType]["name"]+" X"+data[eventType]["amount"]);
-    }else{
+    if (eventType.indexOf("tip") != -1) {
+        $(".main-container").html(data[eventType]["name"] + " " + data[eventType]["amount"].toLocaleString(userLocale, {
+            style: 'currency',
+            currency: userCurrency
+        }));
+    } else if (eventType.indexOf("sub") != -1 || eventType.indexOf("cheer") != -1) {
+        $(".main-container").html(data[eventType]["name"] + " X" + data[eventType]["amount"]);
+    } else if (eventType.indexOf("raid") != -1 || eventType.indexOf("host") != -1) {
+        $(".main-container").html(data[eventType]["name"] + " X" + data[eventType]["amount"]);
+    } else {
         $(".main-container").html(data[eventType]["name"]);
     }
+
+    resize();
 });
 
 
-window.addEventListener('onWidgetLoad', function(obj) {
+window.addEventListener('onWidgetLoad', function (obj) {
     const data = obj.detail.session.data;
     userCurrency = obj.detail.currency.code;
     const fieldData = obj.detail.fieldData;
-    eventType=fieldData["eventType"];
-    userLocale=fieldData["locale"];
-    console.log(eventType);
-    console.log(data);
+    eventType = fieldData["eventType"];
+    userLocale = fieldData["locale"];
+    height = $(".holder").height();
     if (!data[eventType]["name"].length) return;
-    if (eventType.indexOf("tip")!=-1){
-        $(".main-container").html(data[eventType]["name"]+" "+data[eventType]["amount"].toLocaleString(userLocale,{style: 'currency',currency:userCurrency}));
-    }else if (eventType.indexOf("sub")!=-1 || eventType.indexOf("cheer")!=-1){
-        $(".main-container").html(data[eventType]["name"]+" X"+data[eventType]["amount"]);
-    }else if (eventType.indexOf("raid")!=-1 || eventType.indexOf("host")!=-1){
-        $(".main-container").html(data[eventType]["name"]+" X"+data[eventType]["amount"]);
-    }else{
+    if (eventType.indexOf("tip") != -1) {
+        $(".main-container").html(data[eventType]["name"] + " " + data[eventType]["amount"].toLocaleString(userLocale, {
+            style: 'currency',
+            currency: userCurrency
+        }));
+    } else if (eventType.indexOf("sub") != -1 || eventType.indexOf("cheer") != -1) {
+        $(".main-container").html(data[eventType]["name"] + " X" + data[eventType]["amount"]);
+    } else if (eventType.indexOf("raid") != -1 || eventType.indexOf("host") != -1) {
+        $(".main-container").html(data[eventType]["name"] + " X" + data[eventType]["amount"]);
+    } else {
         $(".main-container").html(data[eventType]["name"]);
     }
-
+    resize();
 
 });
