f005679f235fbba766a2582e99749e2d2a190cd4
lx <lukasz@klis.org.pl>
2019-10-05 10:29:43 +0200

    Added aggregated data from stream labels Added toLocaleString for every number

--- CustomLabels/widget.js
+++ CustomLabels/widget.js
@@ -1,4 +1,4 @@
-let userCurrency, userLocale, eventType;
+let userCurrency, userLocale, eventType, showName = false, fieldData;
 let height;
 let resize = () => {
     if ("{{dynamicFontSize}}" === "dynamic") {
@@ -13,44 +13,57 @@ let resize = () => {
 
 window.addEventListener('onSessionUpdate', function (obj) {
     const data = obj.detail.session;
-    if (!data[eventType]["name"].length) return;
-    if (eventType.indexOf("tip") !== -1) {
-        $(".main-container").html(data[eventType]["name"] + " " + data[eventType]["amount"].toLocaleString(userLocale, {
-            style: 'currency',
-            currency: userCurrency
-        }));
-    } else if (eventType.indexOf("sub") !== -1 || eventType.indexOf("cheer") !== -1) {
-        $(".main-container").html(data[eventType]["name"] + " X" + data[eventType]["amount"]);
-    } else if (eventType.indexOf("raid") !== -1 || eventType.indexOf("host") !== -1) {
-        $(".main-container").html(data[eventType]["name"] + " X" + data[eventType]["amount"]);
-    } else {
-        $(".main-container").html(data[eventType]["name"]);
-    }
-
+    updateEvent(data);
     resize();
 });
 
+let updateEvent = (data) => {
+    if (showName) {
+        if (!data[eventType]["name"].length) return;
+        if (eventType.indexOf("tip") !== -1) {
+            $(".main-container").html(data[eventType]["name"] + " " + data[eventType]["amount"].toLocaleString(userLocale, {
+                style: 'currency',
+                currency: userCurrency
+            }));
+        } else if (eventType.indexOf("sub") !== -1 || eventType.indexOf("cheer") !== -1) {
+            $(".main-container").html(data[eventType]["name"] + " X" + data[eventType]["amount"]).toLocaleString(userLocale);
+        } else if (eventType.indexOf("raid") !== -1 || eventType.indexOf("host") !== -1) {
+            $(".main-container").html(data[eventType]["name"] + " X" + data[eventType]["amount"]).toLocaleString(userLocale);
+        } else {
+            $(".main-container").html(data[eventType]["name"]);
+        }
+    } else {
+        let count = 0;
+        if (typeof data[eventType] !== 'undefined') {
+            if (fieldData['eventPeriod'] === 'goal' || fieldData['eventType'] === 'cheer' || fieldData['eventType'] === 'tip' || fieldData['eventType'] === 'subscriber-points') {
+                count = data[eventType]['amount'];
+            } else {
+                count = data[eventType]['count'];
+            }
+        }
+        if (fieldData['eventType'] === 'tip') {
+            $(".main-container").html(count.toLocaleString(userLocale, {style: 'currency', currency: userCurrency}));
+        } else {
+            $(".main-container").html(count.toLocaleString(userLocale));
+        }
+    }
+};
 
 window.addEventListener('onWidgetLoad', function (obj) {
     const data = obj.detail.session.data;
     userCurrency = obj.detail.currency.code;
-    const fieldData = obj.detail.fieldData;
-    eventType = fieldData["eventType"];
+    fieldData = obj.detail.fieldData;
+    eventType = fieldData['eventType'] + "-" + fieldData['eventPeriod'];
+    if (fieldData['eventType'].indexOf("-") !== -1) {
+        if (fieldData['eventType'] !== "subscriber-points") {
+            showName = true;
+        }
+        eventType = fieldData['eventType'];
+
+    }
     userLocale = fieldData["locale"];
     height = $(".holder").height();
-    if (!data[eventType]["name"].length) return;
-    if (eventType.indexOf("tip") !== -1) {
-        $(".main-container").html(data[eventType]["name"] + " " + data[eventType]["amount"].toLocaleString(userLocale, {
-            style: 'currency',
-            currency: userCurrency
-        }));
-    } else if (eventType.indexOf("sub") !== -1 || eventType.indexOf("cheer") !== -1) {
-        $(".main-container").html(data[eventType]["name"] + " X" + data[eventType]["amount"]);
-    } else if (eventType.indexOf("raid") !== -1 || eventType.indexOf("host") !== -1) {
-        $(".main-container").html(data[eventType]["name"] + " X" + data[eventType]["amount"]);
-    } else {
-        $(".main-container").html(data[eventType]["name"]);
-    }
+    updateEvent(data);
     resize();
 
 });
