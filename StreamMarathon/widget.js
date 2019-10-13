e72445a27a351fc3fee5d784f53660f39928b916
lx <lukasz@klis.org.pl>
2019-10-13 10:16:28 +0200

    Removed "-1 to disable" from multiplier descriptions Fixed minimum tip/cheer amount Fixed indents in CSS

--- StreamMarathon/widget.js
+++ StreamMarathon/widget.js
@@ -1,21 +1,9 @@
 //MULTIPLIERS:
-let followSeconds = 1,
-    sub1Seconds = 10,
-    sub2Seconds = 20,
-    sub3Seconds = 30,
-    cheerSeconds = 60, //multiplied by every 100 amount of cheer
-    minCheer = 100, // minimum value of cheer
-    tipSeconds = 60, //multiplied by amount of tip, for example [CURRENCY] 3 will add 3 minutes
-    minTip = 1, //minimum amount of tip
-    hostSeconds = 1, //multiplied by amount of viewers from host
-    hostMin = 100,//minimum viewers in host
-    raidSeconds = 1,//multiplied by amount of viewers from raid
-    raidMin = 100, // minimum viewers in raid
-    addOnZero = false;
+let fieldData;
 
 let maxTime = '2040-06-18 10:45'; // Time cap you want to use
 let minTime = '2019-01-29 12:00';
-
+let addOnZero = false;
 
 //Starting to work like a machine does
 
@@ -57,49 +45,40 @@ window.addEventListener('onEventReceived', function (obj) {
             return;
         }
         if (parseInt(data.tier) === 2000) {
-            if (sub2Seconds > 0) countdown(sub2Seconds);
+            if (fieldData.sub2Seconds > 0) countdown(fieldData.sub2Seconds);
         } else if (parseInt(data.tier) === 3000) {
-            if (sub3Seconds > 0) countdown(sub3Seconds);
+            if (fieldData.sub3Seconds > 0) countdown(fieldData.sub3Seconds);
         } else {
-            if (sub1Seconds > 0) countdown(sub1Seconds);
+            if (fieldData.sub1Seconds > 0) countdown(fieldData.sub1Seconds);
         }
 
     } else if (listener === 'host-latest') {
-        if (data['amount'] < hostMin || hostSeconds < 0) {
+        if (data['amount'] < fieldData.hostMin || fieldData.hostSeconds < 0) {
             return;
         }
-        countdown(hostSeconds * data["amount"]);
+        countdown(fieldData.hostSeconds * data["amount"]);
     } else if (listener === 'raid-latest') {
-        if (data['amount'] < raidMin || raidSeconds < 0) {
+        if (data['amount'] < fieldData.raidMin || fieldData.raidSeconds < 0) {
             return;
         }
-        countdown(raidSeconds * data["amount"]);
+        countdown(fieldData.raidSeconds * data["amount"]);
     } else if (listener === 'cheer-latest') {
-        if (data['amount'] < minCheer || cheerSeconds < 0) {
+        if (data['amount'] < fieldData.cheerMin || fieldData.cheerSeconds < 0) {
             return;
         }
-        countdown(parseInt(cheerSeconds * data["amount"] / 100));
+        countdown(parseInt(fieldData.cheerSeconds * data["amount"] / 100));
     } else if (listener === 'tip-latest') {
-        if (data['amount'] < minTip || tipSeconds < 0) {
+        if (data['amount'] < fieldData.tipMin || fieldData.tipSeconds < 0) {
             return;
         }
-        countdown(parseInt(tipSeconds * data["amount"]));
+        countdown(parseInt(fieldData.tipSeconds * data["amount"]));
     }
     saveState();
 
 });
 window.addEventListener('onWidgetLoad', function (obj) {
-    const fieldData = obj.detail.fieldData;
+    fieldData = obj.detail.fieldData;
 
-    followSeconds = fieldData.followSeconds;
-    sub1Seconds = fieldData.sub1Seconds;
-    sub2Seconds = fieldData.sub2Seconds;
-    sub3Seconds = fieldData.sub3Seconds;
-    cheerSeconds = fieldData.cheerSeconds; //multiplied by amount of cheer
-    tipSeconds = fieldData.tipSeconds; //multiplied by amount of tip, for example [CURRENCY] 3 will add 3 minutes
-    hostSeconds = fieldData.hostSeconds;
-    hostMin = fieldData.hostMin;
-    raidMin = fieldData.raidMin;
     maxTime = new Date(fieldData.maxTime);
     minTime = new Date(fieldData.minTime);
     addOnZero = (fieldData.addOnZero === "add");
