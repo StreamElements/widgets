a21f4608caf1e29e556ebf2cbe67f162b32dae8f
lx <lukasz@klis.org.pl>
2019-12-08 16:43:26 +0100

    Segments from custom fields Replaced permission check function Possibility to use image as wheel

--- WheelOfFortune/widget.js
+++ WheelOfFortune/widget.js
@@ -1,41 +1,31 @@
-let segments = [                               // font size and test colour overridden on backrupt segments.
-    {'fillStyle': '#ee1c24', 'text': '300'},
-    {'fillStyle': '#3cb878', 'text': '450'},
-    {'fillStyle': '#f6989d', 'text': '600'},
-    {'fillStyle': '#00aef0', 'text': '750'},
-    {'fillStyle': '#f26522', 'text': '500'},
-    {'fillStyle': '#000000', 'text': 'BANKRUPT', 'textFontSize': 16, 'textFillStyle': '#ffffff'},
-    {'fillStyle': '#e70697', 'text': '3000'},
-    {'fillStyle': '#fff200', 'text': '600'},
-    {'fillStyle': '#f6989d', 'text': '700'},
-    {'fillStyle': '#ee1c24', 'text': '350'},
-    {'fillStyle': '#3cb878', 'text': '500'},
-    {'fillStyle': '#f26522', 'text': '800'},
-    {'fillStyle': '#a186be', 'text': '300'},
-    {'fillStyle': '#fff200', 'text': '400'},
-    {'fillStyle': '#00aef0', 'text': '650'},
-    {'fillStyle': '#ee1c24', 'text': '1000'},
-    {'fillStyle': '#f6989d', 'text': '500'},
-    {'fillStyle': '#f26522', 'text': '400'},
-    {'fillStyle': '#3cb878', 'text': '900'},
-    {'fillStyle': '#000000', 'text': 'BANKRUPT', 'textFontSize': 16, 'textFillStyle': '#ffffff'},
-    {'fillStyle': '#a186be', 'text': '600'},
-    {'fillStyle': '#fff200', 'text': '700'},
-    {'fillStyle': '#00aef0', 'text': '800'},
-    {'fillStyle': '#ffffff', 'text': 'LOOSE TURN', 'textFontSize': 12}
-];
-let theWheel, channelName, spinCommand, accessLevel, cooldown, spins;
+let theWheel, channelName, spinCommand, fieldData, cooldown, spins;
+
+let checkPrivileges = (data) => {
+    let required = fieldData.privileges;
+    let userState = {
+        'mod': parseInt(data.tags.mod),
+        'sub': parseInt(data.tags.subscriber),
+        'vip': (data.tags.badges.indexOf("vip") !== -1),
+        'badges': {
+            'broadcaster': (data.userId === data.tags['room-id']),
+        }
+    };
+    if (userState.badges.broadcaster) return true;
+    else if (required === "mods" && userState.mod) return true;
+    else if (required === "vips" && (userState.mod || userState.vip)) return true;
+    else if (required === "subs" && (userState.mod || userState.vip || userState.sub)) return true;
+    else if (required === "everybody") return true;
+    else return false;
+};
+
 window.addEventListener('onEventReceived', function (obj) {
     if (obj.detail.listener !== "message") return;
     let data = obj.detail.event.data;
-    data['tags']['mod'] = parseInt(data['tags']['mod']);
-    let message = data["text"];
-    let user = data["nick"];
-    if (message.toLowerCase() !== spinCommand.toLowerCase()) return;
+    if (data["text"].toLowerCase() !== spinCommand.toLowerCase()) return;
     if (wheelSpinning) return;
-    if (accessLevel === 'broadcaster' && channelName !== user) return;
-    if (accessLevel === 'mods' && !(channelName === user || data['tags']['mod'])) return;
-
+    if (!checkPrivileges(data)) {
+        return;
+    }
     startSpin();
     setTimeout(
         function () {
@@ -46,31 +36,66 @@ window.addEventListener('onEventReceived', function (obj) {
 
 window.addEventListener('onWidgetLoad', function (obj) {
     channelName = obj["detail"]["channel"]["username"];
-    const fieldData = obj.detail.fieldData;
+    fieldData = obj.detail.fieldData;
     spinCommand = fieldData['spinCommand'];
-    accessLevel = fieldData['access'];
     cooldown = fieldData['duration'];
     spins = fieldData['spins'];
-    theWheel = new Winwheel({
-        'outerRadius': fieldData['wheelSize'] / 2,        // Set outer radius so wheel fits inside the background.
-        'innerRadius': fieldData['innerRadius'],         // Make wheel hollow so segments don't go all way to center.
-        'textFontSize': fieldData['textSize'],         // Set default font size for the segments.
-        'textOrientation': 'vertical', // Make text vertial so goes down from the outside of wheel.
-        'textAlignment': 'outer',    // Align text to outside of wheel.
-        'numSegments': segments.length,         // Specify number of segments.
-        'segments': segments,          // Define segments including colour and text.
-        'pins':
-            {
-                'number': fieldData['pins'],
-            },
-        'animation':           // Specify the animation to use.
-            {
-                'type': 'spinToStop',
-                'duration': cooldown,     // Duration in seconds.
-                'spins': spins     // Default number of complete spins.
-                //'callbackFinished' : 'spinEnd()'
-            }
-    });
+    let segments = [];
+    let tmpsegments = fieldData.segments.replace(" ", "").split(",");
+    let tmpcolors = fieldData.segmentColors.toLowerCase().replace(" ", "").split(",");
+    for (let i in fieldData.segments.replace(" ", "").split(",")) {
+        segments.push({'text': tmpsegments[i], 'fillStyle': tmpcolors[i]});
+    }
+    if (fieldData.displayImage) {
+        theWheel = new Winwheel({
+            'drawMode': 'image',
+            'outerRadius': fieldData['wheelSize'] / 2,        // Set outer radius so wheel fits inside the background.
+            'innerRadius': fieldData['innerRadius'],         // Make wheel hollow so segments don't go all way to center.
+            'textFontSize': fieldData['textSize'],         // Set default font size for the segments.
+            'textOrientation': 'vertical', // Make text vertial so goes down from the outside of wheel.
+            'textAlignment': 'outer',    // Align text to outside of wheel.
+            'numSegments': segments.length,         // Specify number of segments.
+            'segments': segments,          // Define segments including colour and text.
+            'pins':
+                {
+                    'number': fieldData['pins'],
+                },
+            'animation':           // Specify the animation to use.
+                {
+                    'type': 'spinToStop',
+                    'duration': cooldown,     // Duration in seconds.
+                    'spins': spins     // Default number of complete spins.
+                    //'callbackFinished' : 'spinEnd()'
+                }
+        });
+    } else {
+        theWheel = new Winwheel({
+            'outerRadius': fieldData['wheelSize'] / 2,        // Set outer radius so wheel fits inside the background.
+            'innerRadius': fieldData['innerRadius'],         // Make wheel hollow so segments don't go all way to center.
+            'textFontSize': fieldData['textSize'],         // Set default font size for the segments.
+            'textOrientation': 'vertical', // Make text vertial so goes down from the outside of wheel.
+            'textAlignment': 'outer',    // Align text to outside of wheel.
+            'numSegments': segments.length,         // Specify number of segments.
+            'segments': segments,          // Define segments including colour and text.
+            'pins':
+                {
+                    'number': fieldData['pins'],
+                },
+            'animation':           // Specify the animation to use.
+                {
+                    'type': 'spinToStop',
+                    'duration': cooldown,     // Duration in seconds.
+                    'spins': spins     // Default number of complete spins.
+                    //'callbackFinished' : 'spinEnd()'
+                }
+        });
+    }
+    let loadedImg = new Image();
+    loadedImg.onload = function () {
+        theWheel.wheelImage = loadedImg;    // Make wheelImage equal the loaded image object.
+        theWheel.draw();                    // Also call draw function to render the wheel.
+    };
+    loadedImg.src = fieldData.wheelImage;
 });
 
 // Vars used by the code in this page to do power controls.
