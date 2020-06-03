1d1ca2fa6e3ffb88e3bc0b34496357d7d28a71c6
lx <lukasz@klis.org.pl>
2020-06-03 09:58:51 +0200

    Refactor, cleanup, fix for change from period to interval

--- CustomTopList/widget.js
+++ CustomTopList/widget.js
@@ -24,84 +24,36 @@ window.addEventListener('onWidgetLoad', function (obj) {
     limitQ = parseInt(fieldData.limit);
     offsetQ = parseInt(fieldData.offset);
     userLocale = fieldData.locale;
-
+    channelId = obj.detail.channel.id;
     userCurrency = obj.detail.currency.code;
-    let getChannelId = {
-        method: 'GET',
-        url: 'https://api.streamelements.com/kappa/v2/channels/' + channelName + '/',
-    };
-    makeRequest(getChannelId)
-        .then(obj => {
-            obj = JSON.parse(obj);
-            channelId = obj._id;
-            displayPeople();
-        });
+    displayPeople();
 });
 
 
 function displayPeople() {
 
-    let newDataRequest = {
-        method: 'GET',
-        url: `https://api.streamelements.com/kappa/v2/sessions/${channelId}/top?limit=${limitQ}&offset=${offsetQ}&period=${periodQ}&type=${typeQ}`,
+    fetch(`https://api.streamelements.com/kappa/v2/sessions/${channelId}/top?limit=${limitQ}&offset=${offsetQ}&interval=${periodQ}&type=${typeQ}`, {
         headers: {
             'Authorization': 'apikey ' + apiKey
         }
-    };
-    makeRequest(newDataRequest)
-        .then(obj => {
-            obj = JSON.parse(obj);
-            $(".main-container").html('');
-            obj.forEach(function (element) {
-                    if (typeQ === "tip") {
-                        element.total = element.total.toLocaleString(userLocale, {
-                            style: 'currency',
-                            currency: userCurrency
-                        });
+    }).then(response => response.json()).then(obj => {
+        $(".main-container").html('');
+        obj.forEach(function (element) {
+                if (typeQ === "tip") {
+                    element.total = element.total.toLocaleString(userLocale, {
+                        style: 'currency',
+                        currency: userCurrency
+                    });
 
-                    }
-                    $(".main-container").append(`
+                }
+                $(".main-container").append(`
 <div class="user-row">
 <span class="username">${element.username}</span> - <span class="amount">${element.total}</span>
 </div>`);
-                }
-            );
-        });
-
-}
-
-function makeRequest(opts) {
-    return new Promise(function (resolve, reject) {
-        let xhr = new XMLHttpRequest();
-        xhr.open(opts.method, opts.url);
-        xhr.onload = function () {
-            if (this.status >= 200 && this.status < 300) {
-                resolve(xhr.response);
-            } else {
-                reject({
-                    status: this.status,
-                    statusText: xhr.statusText
-                });
             }
-        };
-        xhr.onerror = function () {
-            reject({
-                status: this.status,
-                statusText: xhr.statusText
-            });
-        };
-        if (opts.headers) {
-            Object.keys(opts.headers).forEach(function (key) {
-                xhr.setRequestHeader(key, opts.headers[key]);
-            });
-        }
-        let params = opts.params;
-        if (params && typeof params === 'object') {
-            params = Object.keys(params).map(function (key) {
-                return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
-            }).join('&');
-        }
-        xhr.send(params);
-    });
+        );
+    })
+
+
 }
 
