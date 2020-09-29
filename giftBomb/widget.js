6185e5d0b0f5a72542dfdba5d098d541d7b116be
lx <lukasz@klis.org.pl>
2020-09-29 09:06:51 +0200

    giftBomb: - Added custom fields - Fixed issue with sender name instead of display name

--- giftBomb/widget.js
+++ giftBomb/widget.js
@@ -1,36 +1,63 @@
 let userCurrency;
+let userCurrency;
+let fieldData;
 let animation;
 let minAmount = 20;
 let displayTime;
 let displayGifter;
 let subsQueue = {};
 let tmpQueue = [];
+let skippable = ["bot:counter", "event:test", "event:skip", "message"];
 window.addEventListener('onEventReceived', function (obj) {
     const listener = obj.detail.listener;
+    console.log(obj.detail);
     const data = obj.detail.event;
-    if (listener !== 'subscriber-latest') return;
-    if (data.gifted !== true) return;
-    if (data.bulkGifted && data.amount >= minAmount) {
-        subsQueue[data.sender] = {
+    console.log(`RECEIVED ${listener}`);
+    if (skippable.indexOf(listener) !== -1) return;
+
+    if (listener !== 'subscriber-latest') {
+        console.log("RESUMING NON SUB");
+        SE_API.resumeQueue();
+        return;
+    }
+    if (data.bulkGifted !== true && !data.gifted) {
+        console.log("RESUMING NON GIFT");
+        SE_API.resumeQueue();
+        return;
+    }
+
+    if (data.amount >= minAmount && data.name === data.sender) {
+        // Initial event for community gift
+        subsQueue[data.displayName] = {
             total: data['amount'],
             subs: []
         }
-    } else if (data.gifted === true && typeof subsQueue[data.sender] !== "undefined") {
+        console.log("RESUMING - INTRO MESSAGE");
+
+    } else if (typeof subsQueue[data.sender] !== "undefined") {
+        // Adding new sub
         subsQueue[data.sender].subs.push(data.name);
         if (subsQueue[data.sender].total <= subsQueue[data.sender].subs.length) {
+            console.log("Playing animation");
             displaySubs(subsQueue[data.sender].subs, data.sender)
             delete subsQueue[data.sender];
+            return;
         }
-    }
 
+    }
+    console.log("RESUMING - Part of subbomb");
+    SE_API.resumeQueue();
 });
 
 function displaySubs(subs, gifter) {
     let delayTime = displayTime;
     if (animation === "train") {
-        delayTime = displayTime * subs.length;
+        delayTime = displayTime * (subs.length);
     }
     $('.main-container').queue(function () {
+        let soundAlert = new Audio(fieldData['alertUrl']);
+        soundAlert.volume = parseInt(fieldData['volume']) / 100;
+        soundAlert.play();
         if (displayGifter) {
             let amount = subs.length;
             $(this).html(`<div class="gifter">${gifter} gifted ${amount} subs to community!</div>`);
@@ -90,7 +117,8 @@ function displaySubs(subs, gifter) {
                 let element = `<div id="s${index}" class="subs rainfall" style="top:-50px;left:${left}px;transform:rotate(${rotate}deg)"><div class="animated {animationIn}">${user}</div></div>`;
                 $('.main-container').append(element);
                 setTimeout(function () {
-                    $('#s' + index).css('top', '1500px');
+                    $('#s' + index).css('top', '1300px');
+                    console.log(index)
                 }, time);
 
             });
@@ -110,6 +138,7 @@ function displaySubs(subs, gifter) {
                 let number = index + 1;
                 $('.train').append(`<div class="wagon">${number} ${user}</div>`);
             });
+            delayTime = delayTime / 2 + displayTime * 4;
         }
         setTimeout(function () {
                 $(".gifter").fadeOut();
@@ -117,6 +146,8 @@ function displaySubs(subs, gifter) {
             delayTime * 1000 - 1000);
 
         setTimeout(function () {
+            console.log("RESUME: End of animation");
+            SE_API.resumeQueue();
             $(".main-container").html('');
         }, delayTime * 1000);
         $(this).delay(delayTime * 1000).dequeue();
@@ -125,6 +156,7 @@ function displaySubs(subs, gifter) {
 }
 
 window.addEventListener('onWidgetLoad', function (obj) {
+    fieldData = obj['detail']['fieldData'];
     userCurrency = obj.detail.currency;
     animation = obj.detail.fieldData.animation;
     animationIn = obj.detail.fieldData.animationIn;
@@ -133,5 +165,3 @@ window.addEventListener('onWidgetLoad', function (obj) {
     displayTime = obj.detail.fieldData.displayTime;
     displayGifter = obj.detail.fieldData.displayGifter;
 });
-
-
