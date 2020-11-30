aad48d753f77c9922678e72c2e115de2387d75d1
lx <lukasz@klis.org.pl>
2020-11-30 11:52:09 +0100

    Widget update to use templating variables and gsap

--- TopEventsRotator/widget.js
+++ TopEventsRotator/widget.js
@@ -1,118 +1,69 @@
-let elements = [
-        {
-            type: "tip-session-top-donator",
-            icon: "fa-money-bill",
-            message: "Top Tipper: {name} - ${amount}",
-            emptyMessage: "No tips yet",
-            factor: "amount", //if unset, 0 or empty string - display emptyMessage
-        },
-        {
-            type: "cheer-session-top-donator",
-            icon: "fa-gem",
-            message: "Top cheerer: {name} - {amount} Bits",
-            emptyMessage: "No cheers yet",
-            factor: "amount",
-        },
-        {
-            type: "subscriber-latest",
-            icon: "fa-people",
-            message: "Most recent sub: {name} (X{amount})",
-            emptyMessage: "No subs yet",
-            factor: "name",
-        },
-    ]
-;
+let fieldData, sessionData, timeout, timeline;
+let height;
+const slideTime=parseInt("{cycleTime}");
+const resize = () => {
+    if ("{{dynamicFontSize}}" === "dynamic") {
+        console.log(height);
+        setTimeout(() => {
+            fitty('.dynamic', {
+                minSize: 12,
+                maxSize: height
+            });
+        }, 5);
+    }
+};
+let prepareField = (field) => {
+    if (typeof field === "undefined") return;
+    if (typeof field.data("template") === "undefined") {
+        field.data("template", field.html());
+    }
+    if (field.html()==="") {
+        field.parent().remove();
+        return;
+    }
 
+    field.html(field.data('template').replace(/{([\w\.\-]*)}/g, function (m, key) {
+        key = key.split(".");
 
-let userCurrency;
-let userLocale = "en-US";
-let animationIn = 'bounceIn';
-let animationOut = 'bounceOut';
-let box;
-let amount = 0;
-let next = 0;
-let timeIn = 400;
-let timeDisplay = 2500;
-let timeOut = 500;
+        if (!sessionData.hasOwnProperty(key[0])) return "";
+        let data = sessionData[key[0]]
 
-let slideTime = timeIn + timeDisplay + timeOut;
-
-function showSlide(i) {
-    next++;
-    $(box[i])
-        .addClass(animationIn + ' animated', timeIn)
-        .show(0, timeIn + timeOut)
-        .removeClass(animationIn, timeDisplay)
-        .addClass(animationOut, timeOut)
-        .removeClass(animationOut + " animated", timeOut + 500)
-        .hide(0, timeOut)
-        .queue(function () {
-            if (next >= amount) {
-                next = 0;
-            }
-            showSlide(next);
-            $(this).dequeue();
-        })
-    ;
+        if (!data.hasOwnProperty(key[1])) return "";
+        if (data[key[1]]===0) return "";
+        return data[key[1]];
+    }));
 
 }
-
-function parseData(data) {
-    elements.forEach(function (element) {
-        let text = "";
-        if (typeof data[element.type] === "undefined") {
-            text = element.emptyMessage;
-        } else if (typeof data[element.type][element.factor] === "undefined" || data[element.type][element.factor] === 0 || data[element.type][element.factor] === "") {
-            text = element.emptyMessage;
-        } else {
-            text = element.message.replace(
-                /{(\w*)}/g,
-                function (m, key) {
-                    return data[element.type].hasOwnProperty(key) ? data[element.type][key] : "";
-                }
-            );
-        }
-        $("#" + element.type).html(text);
+window.addEventListener('onSessionUpdate', (obj) => {
+    sessionData = obj.detail.session;
+    $(".dynamic").each(function (index) {
+        prepareField($(this));
     });
+    resize();
+});
+
+const animate = () => {
+    var slides = $('.slide');
+    timeline = new TimelineMax({repeat:-1, repeatDelay:{hideAfter}});
+    timeline.append(TweenMax.staggerTo(slides, 1, {css:{autoAlpha:1}, repeat:1, yoyo:true, repeatDelay:slideTime-2}, slideTime))
 }
 
-window.addEventListener('onSessionUpdate', e => {
-    const data = e.detail.session;
-    parseData(data);
-});
 
 window.addEventListener('onWidgetLoad', function (obj) {
-    let data = obj["detail"]["session"]["data"];
-    const fieldData = obj.detail.fieldData;
-    animationIn = fieldData['animationIn'];
-    animationOut = fieldData['animationOut'];
-    timeIn = fieldData['timeIn'];
-    timeDisplay = fieldData['timeDisplay'];
-    timeOut = fieldData['timeOut'];
-
-
-    userCurrency = obj.detail.currency;
-
-    slideTime = timeIn + timeDisplay + timeOut;
-    let duplicateCheck = [];
-    elements.forEach(function (element) {
-        if (duplicateCheck.indexOf(element.type) === -1) {
-            $(".container").append(`
-    <div class="mySlides">
-        <div class="icon">
-            <span class="fa ${element.icon}"> </span>
-        </div>
-        <div class="username">
-           <span id="${element.type}"></span>
-        </div>
-    </div>`);
-            duplicateCheck.push(element.type);
-        }
-
+    sessionData = obj.detail.session.data;
+    fieldData = obj.detail.fieldData;
+    $(".dynamic").each(function (index) {
+        prepareField($(this));
     });
-    box = $(".mySlides");
-    parseData(data);
-
-    amount = box.length;
-    showSlide(next);
+    animate();
+    resize();
 });
+$('.dynamic').each(function() {
+    var $this = $(this);
+    if($this.html().replace(/\s|&nbsp;/g, '').length == 0)
+        $this.parent().parent().remove();
+});
+height=$(".slide").height();
+$(".image").width(height);
+$(".text").width($(".slide").width()-height)
+
